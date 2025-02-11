3d4412ef132f08a23c1a839566b228379ff0c8da
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-11 23:37:12 +0530

    changes frontend

--- servers.tf
+++ servers.tf
@@ -1,3 +1,13 @@
+data "aws_ami" "centos" {
+  owners = ["973714476881"]
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+}
+
+output "ami" {
+    value = data.aws_ami.centos.image_id
+}
+
 resource "aws_instance" "frontend" {
   ami = "ami-0b4f379183e5706b9"
   instance_type = "t2.micro"
