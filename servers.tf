9790c80ce03b51cee37376020a14c6f94ecc8159
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-14 10:09:53 +0530

    count fruits

--- servers.tf
+++ servers.tf
@@ -12,7 +12,7 @@ variable "components" {
   default = {
     frontend = {
       name          = "frontend"
-      Instance_type = "t3.small"
+      instance_type = "t3.small"
     }
     mongodb = {
       name          = "mongodb"
@@ -20,42 +20,42 @@ variable "components" {
     }
     catalogue = {
       name          = "catalogue"
-      Instance_type = "t3.small"
+      instance_type = "t3.small"
     }
     redis = {
       name          = "redis"
-      Instance_type = "t3.small"
+      instance_type = "t3.small"
     }
     user = {
       name          = "user"
-      Instance_type = "t3.micro"
+      instance_type = "t3.micro"
     }
     cart = {
       name          = "cart"
-      Instance_type = "t3.micro"
+      instance_type = "t3.micro"
     }
     mysql = {
       name          = "mysql"
-      Instance_type = "t3.small"
+      instance_type = "t3.small"
     }
     shipping = {
       name          = "shipping"
-      Instance_type = "t3.medium"
+      instance_type = "t3.medium"
     }
     rabbitmq = {
       name          = "rabbitmq"
-      Instance_type = "t3.small"
+      instance_type = "t3.small"
     }
     payment = {
       name          = "payment"
-      Instance_type = "t3.small"
+      instance_type = "t3.small"
     }
   }
 }
 resource "aws_instance" "instance" {
   for_each               = var.components
   ami                    = data.aws_ami.centos.image_id
-  instance_type          = each.value["instance_type"]
+  instance_type          = each.value ["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.launch-wizard-1.id]
 
   tags = {
