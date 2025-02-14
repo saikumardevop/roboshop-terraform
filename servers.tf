6515e1225820e6a5a8dcd8b66f648a48efd0d45a
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-14 10:45:55 +0530

    count fruits

--- servers.tf
+++ servers.tf
@@ -63,12 +63,11 @@ resource "aws_instance" "instance" {
     }
 }
 
-//
-# resource "aws_route53_record" "record" {
-#   for_each    = var.components
-#   zone_id = "Z0850283ZZAF9MM79867"
-#   name    = ${each.value["name"]}saikumar22.store
-#   type    = "A"
-#   ttl     = 30
-#   records = [aws_instance[each.value["name"]].private_ip]
-# }
+resource "aws_route53_record" "record" {
+  for_each    = var.components
+  zone_id     = "Z0850283ZZAF9MM79867"
+  name        = ${each.value["name"]}saikumar22.store
+  type        = "A"
+  ttl         = 30
+  records     = [aws_instance[each.value["name"]].private_ip]
+}
