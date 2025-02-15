aba5859e3e0274404c3c1fb6ba899dd01c12ae91
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-15 15:38:22 +0530

    count fruits

--- env-prod/state.tfvars
+++ env-prod/state.tfvars
@@ -1,2 +1,3 @@
 bucket = "terraform-b9"
 key    = "roboshop/dev/terraform.tfstate"
+region = "us-east-1"
--- state.tf
+++ state.tf
@@ -1,7 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-b9"
-    key    = "roboshop/dev/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+ bucket = "terraform-b9"
+ key    = "roboshop/dev/terraform.tfstate"
+ region = "us-east-1"
