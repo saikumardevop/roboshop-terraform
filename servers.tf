630fb87dae4fc91f7e771e61ea5d8bd751d1e8b6
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-15 15:25:45 +0530

    count fruits

--- servers.tf
+++ servers.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "instance" {
 }
 
 resource "aws_route53_record" "record" {
-  for_each    = var.components
+  for_each              = var.components
   zone_id     = "Z0850283ZZAF9MM79867"
   name        = "${each.value["name"]}.saikumar22.store"
   type        = "A"
