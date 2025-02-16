f64a6350c5b532327daaa87d468b731a289e9453
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-02-16 16:55:35 +0530

    sample

--- sarver.tf
+++ sarver.tf
@@ -12,55 +12,8 @@ data "aws_security_group" "launch-wizard-6" {
 #   default = "t2.micro"
 #   }
 
-variable "components" {
-  default ={
-    frontend = {
-      name          = "frontend"
-      instance_type = "t2.micro"
-    }
-    mongodb = {
-      name          = "mongodb"
-      instance_type = "t2.micro"
-    }
-    catalogue = {
-      name          = "catalogue"
-      instance_type = "t3.medium"
-    }
-    redis = {
-      name          = "redis"
-      instance_type = "t3.micro"
-    }
-    user = {
-      name          = "user"
-      instance_type = "t3.micro"
-    }
-    cart = {
-      name          = "cart"
-      instance_type = "t3.micro"
-    }
-    mysql = {
-      name          = "mysql"
-      instance_type = "t3.small"
-      password      = "RoboShop@1"
-    }
-    shipping = {
-      name          = "shipping"
-      instance_type = "t3.medium"
-      password      = "RoboShop@1"
-    }
-    rabbitmq = {
-      name          = "rabbitmq"
-      instance_type = "t2.micro"
-      password      = "roboshop123"
+variable "components" {}
 
-    }
-    payment = {
-      name          = "payment"
-      instance_type = "t3.micro"
-      password      = "roboshop123"
-    }
-  }
-}
 
 resource "aws_instance" "instance" {
   for_each               = var.components
