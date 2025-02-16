8aefe944e874e54a5c1188c9724889fe7edeb674
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-02-16 13:10:03 +0530

    sample

--- sarver.tf
+++ sarver.tf
@@ -1,3 +1,9 @@
+data "aws_ami" "centos"{
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
+
 resource "aws_instance" "frontend" {
   ami           = "ami-0b4f379183e5706b9"
   instance_type = "t2.micro"
