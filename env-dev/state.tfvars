519543efda0f0da99c03eb05f0ae296bf5e65bb3
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-02-16 16:42:16 +0530

    sample

---
+++ env-dev/state.tfvars
@@ -0,0 +1,3 @@
+bucket = "terraform-h76"
+key = "roboshop/dev/terraform.tfstate"
+region = "us-east-1"
--- state.tf
+++ state.tf
@@ -1,7 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket = "terraform-h76"
-    key = "roboshop/dev/terraform.tfstate"
-    region = "us-east-1"
-  }
+  backend "s3" {}
 }
