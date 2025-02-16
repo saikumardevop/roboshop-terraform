8f67589f660182663078b8b9c1cd1f47a921bcc4
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-02-16 14:53:30 +0530

    sample

--- sarver.tf
+++ sarver.tf
@@ -12,20 +12,25 @@ variable "instance_type" {
   default = "t2.micro"
 }
 
+variable "components" {
+  default = [ "frontend,mongodb,catalogue" ]
+}
+
 resource "aws_instance" "frontend" {
+  count =length(var.components)
   ami                    = data.aws_ami.centos.image_id
   instance_type          =var.instance_type
   vpc_security_group_ids = [data.aws_security_group.launch-wizard-6.id]
 
   tags = {
-    Name = "frontend"
+    Name = var.components [count.index]
   }
 }
 
-resource "aws_route53_record" "frontend" {
-  zone_id = "Z07633651VJKTEQ867N3J"
-  name    = "frontend-dev.rdevopsb72.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.frontend.private_ip]
-}
+# resource "aws_route53_record" "frontend" {
+#   zone_id = "Z07633651VJKTEQ867N3J"
+#   name    = "frontend-dev.rdevopsb72.online"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.frontend.private_ip]
+# }
