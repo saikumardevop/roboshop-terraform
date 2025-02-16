5425a4f7959c1b8807024ac17076d933fcdcab7e
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-02-16 17:03:22 +0530

    sample

--- sarver.tf
+++ sarver.tf
@@ -1,20 +1,3 @@
-data "aws_ami" "centos"{
-  owners      = ["973714476881"]
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-}
-
-data "aws_security_group" "launch-wizard-6" {
-  name = "launch-wizard-6"
-}
-
-# variable "instance_type" {
-#   default = "t2.micro"
-#   }
-
-variable "components" {}
-
-
 resource "aws_instance" "instance" {
   for_each               = var.components
   ami                    = data.aws_ami.centos.image_id
