04ce47611ba898f7f69773ce10ea8f26f3738a22
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-02-16 14:33:52 +0530

    sample

--- sarver.tf
+++ sarver.tf
@@ -4,14 +4,28 @@ data "aws_ami" "centos"{
   name_regex  = "Centos-8-DevOps-Practice"
 }
 
+data "aws_security_group" "launch-wizard-6" {
+  name = "launch-wizard-6"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
 resource "aws_instance" "frontend" {
-  ami           = "ami-0b4f379183e5706b9"
-  instance_type = "t2.micro"
+  ami                    = data.aws_ami.centos.image_id
+  instance_type          =var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.launch-wizard-6.id]
 
   tags = {
     Name = "frontend"
   }
 }
-output "frontend" {
-  value = aws_instance.frontend.public_ip
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z07633651VJKTEQ867N3J"
+  name    = "frontend-dev.rdevopsb72.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
 }
