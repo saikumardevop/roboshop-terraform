8e0ae6fe691891a698a61a226d605dd0fe760fbc
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-02-17 12:55:18 +0530

    sample

--- sarver.tf
+++ sarver.tf
@@ -3,7 +3,7 @@ module "servers" {
 
   source         = "./module"
   component_name = each.value["name"]
-  env            = var.env
+  env   = var.env
   instance_type  = each.value["instance_type"]
   password = lookup(each.value, "password", "null")
 }
