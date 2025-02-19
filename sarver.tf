26cc9c0c5cf39cd659b32aa41bec127b7f93f72d
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-19 23:07:56 +0530

    count fruits

--- sarver.tf
+++ sarver.tf
@@ -11,14 +11,15 @@
 
 
  resource "aws_instance" "instance" {
-   for_each    = var.components
-   ami                    = data.aws_ami.centos.image_id
-   instance_type          = each.value["instance_type"]
+   for_each      = var.components
+   ami           = data.aws_ami.centos.image_id
+   instance_type = each.value["instance_type"]
    vpc_security_group_ids = [data.aws_security_group.launch-wizard-5.id]
 
    tags = {
      Name = each.value["name"]
    }
+ }
 
 
  provisioner "remote-exec" {
