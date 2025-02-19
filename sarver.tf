3f68f2b2b7a3b9d5729c1c48fb1efab24c74dd4a
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-19 17:05:27 +0530

    count fruits

--- sarver.tf
+++ sarver.tf
@@ -40,7 +40,7 @@
 resource "aws_route53_record" "records" {
   for_each             = var.components
   zone_id  = "Z082431210MK90HJCBQI2"
-  name     = "${each.value["name"]}-dev.saikumar22.store"
+  name     = "${each.value["name"]}.saikumar22.store"
   type     = "A"
   ttl      = 30
   records  = [aws_instance.instance[each.value["name"]].private_ip]
