a5aa8dcdfb3f339d87a205a1ace4db252c2f2514
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-19 16:20:47 +0530

    count fruits

--- sarver.tf
+++ sarver.tf
@@ -8,16 +8,34 @@
 #   password = lookup(each.value, "password", "null")
 # }
 
-resource "aws_instance" "instance" {
-  for_each = var.components
-  ami                    = data.aws_ami.centos.image_id
-  instance_type          = each.value["instance_type"]
-  vpc_security_group_ids = [data.aws_security_group.launch-wizard-1.id]
+ resource "aws_instance" "instance" {
+   for_each = var.components
+   ami                    = data.aws_ami.centos.image_id
+   instance_type          = each.value["instance_type"]
+   vpc_security_group_ids = [data.aws_security_group.launch-wizard-1.id]
 
-  tags = {
-    Name = each.value["name"]
-  }
+   tags = {
+     Name = each.value["name"]
+   }
+
+ provisioner "remote-exec" {
+
+   connection {
+     type     = "ssh"
+     user     = "centos"
+     password = "DevOps321"
+     host     = self.private_ip
+   }
+
+   inline = [
+     "rm -rf roboshop-shell",
+     "git clone https://github.com/saikumardevop/roboshop-shell.git",
+     "cd roboshop-shell",
+     "sudo bash ${each.value["name"]}.sh"
+   ]
+ }
 }
+
 
 resource "aws_route53_record" "records" {
   for_each             = var.components
