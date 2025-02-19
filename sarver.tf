0505d7527cdfe6548dcb30659078ef76514f6088
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-19 13:12:07 +0530

    count fruits

--- sarver.tf
+++ sarver.tf
@@ -8,21 +8,6 @@
 #   password = lookup(each.value, "password", "null")
 # }
 
-
-data "aws_ami" "centos" {
-  owners = ["973714476881"]
-  most_recent = true
-  name_regex = "Centos-8-DevOps-Practice"
-}
-
-
-data "aws_security_group" "launch-wizard-1" {
-  name = "launch-wizard-1"
-}
-
-
-variable "components" {}
-
 resource "aws_instance" "instance" {
   for_each = var.components
   ami                    = data.aws_ami.centos.image_id
