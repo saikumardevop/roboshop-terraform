39c6b1b195000b4ae4741cdcffc3d61ea9b31630
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-19 22:32:04 +0530

    count fruits

--- sarver.tf
+++ sarver.tf
@@ -19,56 +19,7 @@ data "aws_security_group" "launch-wizard-5" {
   name = "launch-wizard-5"
 }
 
-variable "components" {
-  default ={
-    frontend = {
-      name          = "frontend"
-      instance_type = "t3.micro"
-    }
-    mongodb = {
-      name          = "mongodb"
-      instance_type = "t3.micro"
-    }
-    catalogue = {
-      name          = "catalogue"
-      instance_type = "t3.medium"
-    }
-    redis = {
-      name          = "redis"
-      instance_type = "t3.micro"
-    }
-    user = {
-      name          = "user"
-      instance_type = "t3.micro"
-    }
-    cart = {
-      name          = "cart"
-      instance_type = "t3.micro"
-    }
-    mysql = {
-      name          = "mysql"
-      instance_type = "t3.small"
-      password      = "RoboShop@1"
-    }
-    shipping = {
-      name          = "shipping"
-      instance_type = "t3.medium"
-      password      = "RoboShop@1"
-    }
-    rabbitmq = {
-      name          = "rabbitmq"
-      instance_type = "t2.micro"
-      password      = "roboshop123"
-
-    }
-    payment = {
-      name          = "payment"
-      instance_type = "t3.micro"
-      password      = "roboshop123"
-    }
-  }
-}
-
+variable "components" {}
 
  resource "aws_instance" "instance" {
    for_each      = var.components
