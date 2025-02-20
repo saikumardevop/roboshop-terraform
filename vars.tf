9cdae2231daee5493ea07efdad1579c736872a21
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-20 23:06:47 +0530

    count fruits

--- vars.tf
+++ vars.tf
@@ -1,2 +1,11 @@
 variable "env" {}
-variable "components" {}
+variable "components" {
+  frontend = {
+    name          = "frontend"
+    instance_type = "t3.micro"
+  }
+  mongodb = {
+    name          = "mongodb"
+    instance_type = "t3.micro"
+  }
+}
