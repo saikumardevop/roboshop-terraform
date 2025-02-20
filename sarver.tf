e84935d1faad613a4e09db98faaf2b71d357412e
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-20 12:40:06 +0530

    count fruits

--- sarver.tf
+++ sarver.tf
@@ -1,50 +1,12 @@
-# module "servers" {
-#   for_each = var.components
-#
-#   source         = "./module"
-#   component_name = each.value["name"]
-#   env   = var.env
-#   instance_type  = each.value["instance_type"]
-#   password = lookup(each.value, "password", "null")
-# }
+module "servers" {
+  for_each = var.components
 
-
-
- resource "aws_instance" "instance" {
-   ami           = data.aws_ami.centos.image_id
-   instance_type = each.value["instance_type"]
-   vpc_security_group_ids = [data.aws_security_group.launch-wizard-5.id]
-
-   tags = {
-     Name = each.value["name"]
-   }
- }
-
-
- provisioner "remote-exec" {
-
-   connection {
-     type     = "ssh"
-     user     = "centos"
-     password = "DevOps321"
-     host     = self.private_ip
-   }
-
-   inline = [
-     "rm -rf roboshop-shell1",
-     "git clone https://github.com/saikumardevop/roboshop-shell1.git",
-     "cd roboshop-shell1",
-     "sudo bash ${each.value["name"]}.sh"
-   ]
- }
+  source         = "./module"
+  component_name = each.value["name"]
+  env   = var.env
+  instance_type  = each.value["instance_type"]
+  password = lookup(each.value, "password", "null")
 }
 
 
 
-resource "aws_route53_record" "records" {
-  zone_id   = "Z082431210MK90HJCBQI2"
-  name      = "${each.value["name"]}-dev.saikumar22.store"
-  type      = "A"
-  ttl       = 30
-  records   = [aws_instance.instance[each.value["name"]].private_ip]
-}
