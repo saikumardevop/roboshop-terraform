df72d59c8afc66398962902f3e9446334d0a37b7
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-22 17:25:59 +0530

    count fruits

--- servers.tf
+++ servers.tf
@@ -9,7 +9,7 @@
 
 
 resource "aws_instance" "instance" {
-  for_each               = var.components
+  for_each = var.components
   ami                    = data.aws_ami.centos.image_id
   instance_type          = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.launch-wizard-5.id]
