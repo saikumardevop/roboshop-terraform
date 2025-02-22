cc96ead3b54675ae80b6eec8cde28b5cf1683dd9
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-22 16:19:10 +0530

    count fruits

--- module/main.tf
+++ module/main.tf
@@ -1,41 +1,41 @@
-resource "aws_instance" "instance" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [data.aws_security_group.launch-wizard-5.id]
-
-  tags = {
-    Name =  local.name
-
-  }
-}
-
-resource "null_resource" "provisioner" {
-
-  depends_on = [aws_instance.instance, aws_route53_record.records]
-  provisioner "remote-exec" {
-
-    connection {
-      type     = "ssh"
-      user     = "centos"
-      password = "DevOps321"
-      host     = aws_instance.instance.private_ip
-    }
-
-    inline = [
-      "rm -rf roboshop-shell1",
-      "git clone https://github.com/saikumardevop/roboshop-shell1.git",
-      "cd roboshop-shell1",
-      "sudo bash ${var.component_name}.sh ${var.password}"
-    ]
-  }
-}
-
-
-resource "aws_route53_record" "records" {
-zone_id   = "Z082431210MK90HJCBQI2"
-name      = "${var.component_name}-dev.saikumar22.store"
-type      = "A"
-ttl       = 30
-records   = [aws_instance.instance.private_ip]
-}
-
+# resource "aws_instance" "instance" {
+#   ami           = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = [data.aws_security_group.launch-wizard-5.id]
+#
+#   tags = {
+#     Name =  local.name
+#
+#   }
+# }
+#
+# resource "null_resource" "provisioner" {
+#
+#   depends_on = [aws_instance.instance, aws_route53_record.records]
+#   provisioner "remote-exec" {
+#
+#     connection {
+#       type     = "ssh"
+#       user     = "centos"
+#       password = "DevOps321"
+#       host     = aws_instance.instance.private_ip
+#     }
+#
+#     inline = [
+#       "rm -rf roboshop-shell1",
+#       "git clone https://github.com/saikumardevop/roboshop-shell1.git",
+#       "cd roboshop-shell1",
+#       "sudo bash ${var.component_name}.sh ${var.password}"
+#     ]
+#   }
+# }
+#
+#
+# resource "aws_route53_record" "records" {
+# zone_id   = "Z082431210MK90HJCBQI2"
+# name      = "${var.component_name}-dev.saikumar22.store"
+# type      = "A"
+# ttl       = 30
+# records   = [aws_instance.instance.private_ip]
+# }
+#
--- servers.tf
+++ servers.tf
@@ -1,9 +1,54 @@
-module "servers" {
-  for_each = var.components
+# module "servers" {
+#   for_each = var.components
+#
+#   source         = "./module"
+#   component_name = each.value["name"]
+#   env   = var.env
+#   instance_type  = each.value["instance_type"]
+#   password = lookup(each.value, "password", "null"
 
-  source         = "./module"
-  component_name = each.value["name"]
-  env   = var.env
-  instance_type  = each.value["instance_type"]
-  password = lookup(each.value, "password", "null")
+
+resource "aws_instance" "instance" {
+  for_each      =var.components
+  ami           = data.aws_ami.centos.image_id
+  instance_type = each.value["instance_type"]
+  vpc_security_group_ids = [data.aws_security_group.launch-wizard-5.id]
+
+  tags = {
+    Name =  each.value["name"]
+
+  }
 }
+
+resource "null_resource" "provisioner" {
+
+  depends_on = [aws_instance.instance, aws_route53_record.records]
+  for_each   = var.components
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = aws_instance.instance.private_ip
+    }
+
+    inline = [
+      "rm -rf roboshop-shell1",
+      "git clone https://github.com/saikumardevop/roboshop-shell1.git",
+      "cd roboshop-shell1",
+      "sudo bash ${each.value.["name"]}.sh ${each.value["password"]}"
+    ]
+  }
+}
+
+
+resource "aws_route53_record" "records" {
+        for_each       =var.components
+  zone_id   = "Z082431210MK90HJCBQI2"
+  name      = "${each.value["name"]}-dev.saikumar22.store"
+  type      = "A"
+  ttl       = 30
+  records   = [aws_instance.instance[each.value["name"]].private_ip]
+}
+
