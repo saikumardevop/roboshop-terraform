55eceea61541043f5a61dfa475b7afe2696578a6
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-22 16:33:17 +0530

    count fruits

--- servers.tf
+++ servers.tf
@@ -37,7 +37,7 @@ resource "null_resource" "provisioner" {
       "rm -rf roboshop-shell1",
       "git clone https://github.com/saikumardevop/roboshop-shell1.git",
       "cd roboshop-shell1",
-      "sudo bash ${each.value.["name"]}.sh ${each.value["password"]}"
+      "sudo bash ${each.value.["name"]}.sh"
     ]
   }
 }
