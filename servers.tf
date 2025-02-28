b0e4a60f481d0df753f1649e0b8a9f07a0c2a556
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-28 19:01:22 +0530

    vars.yml

--- servers.tf
+++ servers.tf
@@ -17,5 +17,5 @@ module "app-servers" {
   component_name = each.value["name"]
   env            = var.env
   instance_type  = each.value["instance_type"]
-  password       = lookup(each.value, "password", "null")
+  password = lookup(each.value, "password", "null")
 }
